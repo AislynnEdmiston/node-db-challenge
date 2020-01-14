028336c1a9b099c9019ea469406b902050a113de
AislynnEdmiston <davidandaislynn@gmail.com>
2020-01-14 18:43:21 -0500

    added the routes in server, commented out 2 until needed

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const server = express();
 
 const PORT = 4000
 
+server.use('/api/project', project)
+
+// server.use('/api/task', project)
+
+// server.use('/api/resource', project)
 
 server.listen(PORT, () => {
     console.log(`The server is running on PORT: ${PORT}`)
